34e56b58a55294612529b8bc1e6b2f3348e308bb
udomsak <specialclub13@gmail.com>
2021-09-21 17:47:18 +0700

    add category

--- controller/productController.js
+++ controller/productController.js
@@ -21,9 +21,9 @@ const getProductById = async (req, res) => {
 
 const addProduct = async (req, res) => {
     try {
-        const { name, prices, status, quantity, image_url } = req.body
-        const response = await pool.query('INSERT INTO product_items (name, prices, status, quantity, image_url) VALUES ($1, $2, $3, $4, $5) RETURNING *', 
-        [name, prices, status, quantity, image_url])
+        const { name, prices, status, quantity, image_url, category } = req.body
+        const response = await pool.query('INSERT INTO product_items (name, prices, status, quantity, image_url, category) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *', 
+        [name, prices, status, quantity, image_url, category])
         res.status(201).json(response.rows)
       } catch (error) {
           console.log(error)
@@ -34,8 +34,8 @@ const updateProduct = async (req, res) => {
     try {
         const { id } = req.params
         const response = await pool.query(
-            'UPDATE product_items SET name = ($1), prices = ($2), status = ($3), quantity = ($4), image_url = ($5) WHERE id = ($6) RETURNING *',
-        [req.body.name, req.body.prices, req.body.status, req.body.quantity, req.body.image_url, id])
+            'UPDATE product_items SET name = ($1), prices = ($2), status = ($3), quantity = ($4), image_url = ($5), category = ($6) WHERE id = ($7) RETURNING *',
+        [req.body.name, req.body.prices, req.body.status, req.body.quantity, req.body.image_url, req.body.category, id])
         res.status(201).json(response.rows) 
     } catch (error) {
         console.log(error)
